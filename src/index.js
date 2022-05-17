6da963715b38edeb4c67e21ddbffdfbf3314e6ea
Jun1ka10 <64685693+Jun1ka10@users.noreply.github.com>
2022-05-17 23:52:32 +0900

    todo-add finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //入力textの取得とクリア
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  //divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの下に要素を設定
+  div.appendChild(li);
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
